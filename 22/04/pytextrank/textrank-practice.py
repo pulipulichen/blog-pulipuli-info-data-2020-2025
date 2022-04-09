4fd815665efc7aab590c1f44a612ce9e7860da10
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-04-09 15:52:06 +0800

    Update textrank-practice.py

--- 22/04/pytextrank/textrank-practice.py
+++ 22/04/pytextrank/textrank-practice.py
@@ -1,3 +1,9 @@
+# Configuration
+output_phrases_count = 10
+output_sentences_count = 3
+
+# -----------
+
 !python3 -m pip install pytextrank
 !python3 -m spacy download en_core_web_sm
 
@@ -18,7 +24,7 @@ with open('input.txt', 'r') as file:
 doc = nlp(text)
 
 # examine the top-ranked phrases in the document
-path = 'output-chunks.txt'
+path = 'output-phrases.txt'
 f = open(path, 'w')
 
 for phrase in doc._.phrases:
@@ -26,10 +32,7 @@ for phrase in doc._.phrases:
     print("TEXT:\t", phrase.text)
     print("COUNT:\t", phrase.count)
     print("CHUNKS:\t", phrase.chunks)
-    f.write(" ".join(str(item) for item in phrase.chunks))
-    f.write("\n")
 
-f.close()
 
 # -----------------
 # Construct a list of the sentence boundaries with a phrase vector (initialized to empty set) for each...
@@ -43,8 +46,6 @@ sent_bounds = [ [s.start, s.end, set([])] for s in doc.sents ]
 import sys ; sys.path.insert(0, "../")
 !python3 -m pip install icecream
 from icecream import ic
-
-limit_phrases = 4
 
 phrase_id = 0
 unit_vector = []
@@ -65,8 +66,13 @@ for p in doc._.phrases:
 
     phrase_id += 1
 
-    if phrase_id == limit_phrases:
+    f.write(p.text)
+    if phrase_id == output_phrases_count:
         break
+
+    f.write("\n")
+
+f.close()
 
 # -----------------
 # Then normalized...
@@ -107,8 +113,6 @@ sorted(sent_rank.items(), key=itemgetter(1))
 # -----------------
 # Extract the sentences with the lowest distance, up to the limit requested...
 
-limit_sentences = 2
-
 sent_text = {}
 sent_id = 0
 
@@ -126,9 +130,8 @@ for sent_id, rank in sorted(sent_rank.items(), key=itemgetter(1)):
     f.write(sent_text[sent_id])
     num_sent += 1
 
-    if num_sent == limit_sentences:
+    if num_sent == output_sentences_count:
         break
     f.write("\n")
 
 f.close()
-
